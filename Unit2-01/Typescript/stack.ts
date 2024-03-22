98f560fb614ab9b21827e6e28f6399b6e20926b3
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2024-03-22 09:46:34 -0400

    init

--- Unit2-01/Typescript/stack.ts
+++ Unit2-01/Typescript/stack.ts
@@ -1,36 +1,35 @@
 /** 
- *  * This program defines a stack
- *   * By:      Johanna Liu
- *    * Version: 1.0
- *     * Since:   2024-03-03
- *      */
+ * This program defines a stack
+ * By:      Johanna Liu
+ * Version: 1.0
+ * Since:   2024-03-03
+ */
 
 class Stack {
-          //create stack as list
-        //  stackList: string[]
-        //
-        //    constructor() {
-        //        this.stackList = []
-        //          }
-        //
-        //            push(pushedString: string): void {
-        //                // push the value onto the stack
-        //                    this.stackList.push(pushedString)
-        //                      }
-        //
-        //                        showStack(): string {
-        //                            // return the stack
-        //                                let stackValues: string = ""
-        //
-        //                                    for (const value of this.stackList) {
-        //                                          stackValues += value + ", "
-        //                                              }
-        //                                                  stackValues = stackValues.slice(0, -2) // Remove the trailing comma
-        //
-        //                                                      return stackValues
-        //                                                        }
-        //                                                        }
-        //
-        //                                                        // Export the class
-        //                                                        export default Stack;
-        //                                                        
+  //create stack as list
+  stackList: string[]
+
+  constructor() {
+    this.stackList = []
+  }
+
+  push(pushedString: string): void {
+    // push the value onto the stack
+    this.stackList.push(pushedString)
+  }
+
+  showStack(): string {
+    // return the stack
+    let stackValues: string = ""
+
+    for (const value of this.stackList) {
+      stackValues += value + ", "
+    }
+    stackValues = stackValues.slice(0, -2) // Remove the trailing comma
+
+    return stackValues
+  }
+}
+
+// Export the class
+export default Stack;
