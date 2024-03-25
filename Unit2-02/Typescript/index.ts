edf82ee13d780cd4138f886d7b7f1b6ae768484e
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2024-03-25 09:17:32 -0400

    init

--- Unit2-02/Typescript/index.ts
+++ Unit2-02/Typescript/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+/**
+ *
+ * This program uses stacks
+ *
+ * By:      Johanna Liu
+ * Version: 1.0
+ * Since:   2024-03-19
+ */
+
+import TheStack from './stack'
+
+const treatsStack = new TheStack()
+treatsStack.push('chocolate')
+treatsStack.push('chips')
+treatsStack.push('candy')
+
+const numberStack = new TheStack()
+numberStack.push('one')
+numberStack.push('two')
+numberStack.push('three')
+
+console.log(`Treats: ${treatsStack.showStack()}`)
+console.log(`Numbers: ${numberStack.showStack()}`)
