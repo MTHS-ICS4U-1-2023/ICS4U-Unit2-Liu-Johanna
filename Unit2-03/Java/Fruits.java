83cd55a8499900445af21f3c7c5fcad59351916f
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2024-03-30 21:46:11 -0400

    init

--- Unit2-03/Java/Fruits.java
+++ Unit2-03/Java/Fruits.java
@@ -1,1 +1,45 @@
-Fruits!
+/*
+ * This program uses oop
+ *
+ * @author  Johanna Liu
+ * @version 1.0
+ * @since   2024-03-028
+ */
+
+/**
+ * This program uses stacks.
+ */
+final class Fruits {
+
+    /**
+     * Prevent instantiation.
+     * Throw an exception IllegalStateException.
+     * if this ever is called
+     *
+     * @throws IllegalStateException
+     *
+     */
+    private Fruits() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+     * Process.
+     *
+     * @param args No args will be used
+     *
+     */
+    public static void main(final String[] args) {
+        // input
+        final Stack fruits = new Stack();
+        fruits.push("Mango");
+        fruits.push("Kiwi");
+        fruits.push("Papaya");
+        System.out.println("Fruits: " + fruits.showStack());
+        String removedFruit = fruits.pop();
+        System.out.println("Popped fruit item: " + removedFruit);
+        System.out.println("Fruits: " + fruits.showStack());
+
+        System.out.println("\nDone.");
+    }
+}
--- Unit2-03/Java/Number.java
+++ Unit2-03/Java/Number.java
@@ -1,1 +1,45 @@
-number
+/*
+ * This program uses oop
+ *
+ * @author  Johanna Liu
+ * @version 1.0
+ * @since   2024-03-028
+ */
+
+/**
+ * This program uses stacks.
+ */
+final class Number {
+
+    /**
+     * Prevent instantiation.
+     * Throw an exception IllegalStateException.
+     * if this ever is called
+     *
+     * @throws IllegalStateException
+     *
+     */
+    private Number() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+     * Process.
+     *
+     * @param args No args will be used
+     *
+     */
+    public static void main(final String[] args) {
+        // input
+        final Stack numbers = new Stack();
+        numbers.push("One");
+        numbers.push("Two");
+        numbers.push("Three");
+        System.out.println("Numbers: " + numbers.showStack());
+        String removedNumber = numbers.pop();
+        System.out.println("Popped number item: " + removedNumber);
+        System.out.println("Numbers: " + numbers.showStack());
+
+        System.out.println("\nDone.");
+    }
+}
